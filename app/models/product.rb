b7ab276bb451f72ff1337e45319f567b38a53c6e
davidsp <davidsp@gmail.com>
2014-03-17 23:31:29 +0100

    adding relations across models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
-		validates :name, presence: true, length: {minimum: 3}
+	validates :name, presence: true, length: {minimum: 3}
+	has_many :line_items
+	has_many :orders, through :line_items
 
 end
